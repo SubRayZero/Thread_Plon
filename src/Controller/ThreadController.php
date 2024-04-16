6316e2440c97f6f023143a37a7527c25e958b668
SubRayZero <jasmine.draperi.simplon@gmail.com>
2024-04-16 11:40:17 +0200

    First try of details threads it's ok

--- src/Controller/ThreadController.php
+++ src/Controller/ThreadController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\Thread;
 use App\Form\ThreadFormType;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
@@ -21,6 +23,38 @@ class ThreadController extends AbstractController
 
         return $this->render('thread/index.html.twig', [
             'threadForm' => $form,
+        ]);
+    }
+
+    #[Route('/home', name: 'app_home')]
+
+    public function threadAll(EntityManagerInterface $entityManager)
+    {
+
+        $threadRepository = $entityManager->getRepository(Thread::class);
+        $threads = $threadRepository->findAll();
+
+
+        return $this->render('home/index.html.twig', [
+            //'controller_name'=> 'ThreadController',
+            'threads'=> $threads
+        ]);
+    }
+
+
+
+    #[Route('/home/{id}', name: 'app_home_details')]
+
+    public function threadHome($id, EntityManagerInterface $entityManager)
+    {
+
+        $threadRepository = $entityManager->getRepository(Thread::class);
+        $thread = $threadRepository->find($id);
+
+
+        return $this->render('thread/details.html.twig', [
+            'controller_name'=> 'ThreadController',
+            'thread'=> $thread
         ]);
     }
 }
